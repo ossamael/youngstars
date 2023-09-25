9a97cd46ff5f3227fcfc1992b12ff929a3c7cdd8
ossamael <47452109+ossamael@users.noreply.github.com>
2023-09-25 03:40:31 -0700

    Update TestimonialsContent.js

--- src/Components/TestimonialsContent.js
+++ src/Components/TestimonialsContent.js
@@ -3,42 +3,44 @@ import c1 from "../Assets/Images/C1.jpg";
 import c2 from "../Assets/Images/C2.jpg";
 import c3 from "../Assets/Images/C3.jpg";
 import c4 from "../Assets/Images/IMG_9268.jpg";
+import c5 from "../Assets/Images/C5.jpg";
+import c6 from "../Assets/Images/C6.jpg";
 
 export const TestimonialsContent = [
   {
     image: c1,
     text: "Slate helps you see how many more days you need to work to reach your financial goal for the month and year.",
-    name: "Ahmed",
+    name: "Ahmed ALTAMIMI",
     profession: "Smart Car",
   },
   {
     image: c2,
-    text: "Discovering what's an electric circuit by lighting LED's using resistors. Our students accomplish their first task and take confidence",
-    name: "Nehile",
-    profession: "LEDS and Arduino",
+    text: "Discovering what's an electric circuit by lighting LED's using resistors. Our students accomplish their first task.",
+    name: "Nehaile ",
+    profession: "LEDS and Resistor",
   },
   {
     image: c3,
-    text: "Slate helps you see how many more days you need to work to reach your financial goal for the month and year.",
-    name: "Mohammed",
-    profession: "Manger",
+    text: "Building Lego Robots to improve coordination between mind and fingers, then learning to control this robot using code.",
+    name: "Mohammed ALMARZOUQI",
+    profession: "Lego Robots",
   },
   {
     image: c4,
-    text: "Slate helps you see how many more days you need to work to reach your financial goal for the month and year.",
-    name: "Naveed",
-    profession: "Solver",
+    text: "Robot Arm invention, assembling a robotic arm that is controlled using code to move items automatically.",
+    name: "Hemdan ALMARZOUQI",
+    profession: "Robotic Arm",
   },
   {
-    image: c2,
-    text: "Slate helps you see how many more days you need to work to reach your financial goal for the month and year.",
-    name: "Zain",
-    profession: "Freelancer",
+    image: c5,
+    text: "Our young Sheikh learning the basics of circuitry using Arduino and different types of modules.",
+    name: "Ahmed ALTAMIMI ",
+    profession: "Circuitry",
   },
   {
-    image: c3,
-    text: "Slate helps you see how many more days you need to work to reach your financial goal for the month and year.",
-    name: "Bilal",
-    profession: "Writer",
+    image: c6,
+    text: "Working as a team to program and build projects is essential in this modern world we live in.",
+    name: "Hadi ALAMRI",
+    profession: "TeamWork",
   },
 ];
