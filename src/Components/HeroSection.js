854e6ec1953c4b98e8e6172f6c1f29d60c2e3bba
ossamael <47452109+ossamael@users.noreply.github.com>
2024-06-22 12:24:56 +0400

    Update HeroSection.js

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -7,7 +7,7 @@ import "../Styles/HeroSection.css"
 
 const images = [
   { url: "/Images/slider_img_0.jpg" },
-  { url: "/Images/Cover-test.jpeg" },
+  { url: "/Images/Cover-Test.jpeg" },
 ];
 
 function HeroSection({ t }) {
